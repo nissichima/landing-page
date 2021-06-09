31d2b828f052e004e503db9b08ca164b7fdef5aa
nissichima <77025831+nissichima@users.noreply.github.com>
2021-06-08 22:27:35 -0400

    Add files via upload

--- javaland.js
+++ javaland.js
@@ -1,55 +1,21 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Define Global Variables
- * 
-*/
+let anchorSelector = 'a[href^="#"]';
 
+let anchorList = document.querySelectorAll(anchorSelector);
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+//console.log(anchorList);
 
+anchorList.forEach(link => {
+    link.onclick = function(e){
+        // Prevent scrolling if the hash value is blank
+        e.preventDefault();
 
+        //Get the destination to scroll to using the hash property.
+        let destination = document.querySelector(this.hash);
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+        //Scroll to the destination using scrollIntoView method
+        destination.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+});
